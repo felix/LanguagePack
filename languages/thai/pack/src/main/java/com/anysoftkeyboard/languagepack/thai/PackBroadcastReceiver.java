b759ba96ef3a69f47154a4e99af3f93ea0d75cf2
Felix Hanley <felix@userspace.com.au>
2019-01-20 16:54:40 +1100

    Start on Thai pack

---
+++ languages/thai/pack/src/main/java/com/anysoftkeyboard/languagepack/thai/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.thai;
+
+import com.anysoftkeyboard.languagepack.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
